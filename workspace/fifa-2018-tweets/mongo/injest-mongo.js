089faafa1e357264fb72654385c5a4e192811fbd
nelt <nel@flexio.fr>
2020-02-16 14:46:14 +0100

    script comments

--- workspace/fifa-2018-tweets/mongo/injest-mongo.js
+++ workspace/fifa-2018-tweets/mongo/injest-mongo.js
@@ -4,13 +4,30 @@ exports.run = async function (args) {
     const MongoClient = require('mongodb').MongoClient
 
     mogoClient = await MongoClient.connect('mongodb://mongo:27017', { useUnifiedTopology: true })
-    const db = mogoClient.db("fifa_tweets")
 
+    /*
+    Création de la base de données et des collections :
+
+    Pas de commande de création explicite, il suffit d'accéder à une base / collection pour
+    qu'elles soient créées
+     */
+    const db = mogoClient.db("fifa_tweets")
     const tweets = db.collection('tweets')
+
+    /*
+    Création des indexes
+     */
     db.createIndex('tweets', 'id')
     db.createIndex('tweets', 'date')
+    /*
+    Pour l'index sur le champ "tweet", on spécifie son type, le type text.
+    https://docs.mongodb.com/manual/text-search/
+    */
     db.createIndex( 'tweets',  { tweet: "text"} )
 
+    /*
+    Création de la collection "hastags" et de son index
+     */
     const hashtags = db.collection('hashtags')
     db.createIndex('hashtags', 'tag')
     
@@ -25,6 +42,11 @@ exports.run = async function (args) {
         console.info("will read all records from FIFA.csv")
     }
 
+    /*
+    Ouverture d'un flux pour lire le fichier avec la librairie fs.
+    Le flux est ensuite passé (methode pipe) à la librairie csv-parse qui implémente un mécanisme de lecture asynchrone
+    du fichier.
+     */
     let stream = fs.createReadStream('./workspace/fifa-2018-tweets/data-set/FIFA.csv');
     await stream
         .pipe(parse({
@@ -33,10 +55,19 @@ exports.run = async function (args) {
             columns: true
         }))
         .on('readable', async function(){
+            /*
+            Cette fonction est appelée lors que des lignes du fchier CSV sont prètes à être traitées
+             */
             let row
             let lapStart = Date.now()
             while (row = this.read()) {
                 let tags = row.Hashtags ? row.Hashtags.split(',') : [];
+
+                /*
+                Insertion de d'un tweet :
+                    - la variable row contient une ligne du fichier CSV
+                    - on utilise la méthode updateOne pour insérer / mettre à jour le tweet avec l'option upsert (cf. étude de cas)
+                 */
                 await tweets.updateOne(
                     {id: row.ID},
                     {$set: {
@@ -58,6 +89,11 @@ exports.run = async function (args) {
                     {upsert: true}
                 )
                 for (var i = 0; i < tags.length; i++) {
+                    /*
+                    On construit en parrallèle la collection "hashtags" qui contient les compteurs.
+                    Là aussi, on utilise l'option upsert pour insèrer / mettre à jour les compteurs.
+                    On utilise également l'opérateur $inc qui incrémente la valeur d'un champs, ici, le champ "count".
+                     */
                     await hashtags.updateOne(
                         {tag: tags[i]},
                         {$inc: {count: 1}},
